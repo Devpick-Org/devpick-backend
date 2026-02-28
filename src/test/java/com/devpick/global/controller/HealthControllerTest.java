f29331a86165dc69b4fc322390f2f99e6923801c
홍근 <khg9859@gmail.com>
2026-02-28 15:14:14 +0000

    fix: CI 테스트 환경 수정
    - application-test.yml 추가 (test 프로파일)
    - ci.yml MongoDB 서비스 추가, 중복 test 스텝 제거
    - DevpickApplicationTests @ActiveProfiles local→test
    - HealthControllerTest SecurityAutoConfiguration 제외

--- src/test/java/com/devpick/global/controller/HealthControllerTest.java
+++ src/test/java/com/devpick/global/controller/HealthControllerTest.java
@@ -3,13 +3,14 @@ package com.devpick.global.controller;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(HealthController.class)
+@WebMvcTest(value = HealthController.class, excludeAutoConfiguration = SecurityAutoConfiguration.class)
 class HealthControllerTest {
 
     @Autowired
