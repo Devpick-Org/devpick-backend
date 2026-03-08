35bf5fe6504750ab98c1bc6de08c6c95016d2eba
홍근 <khg9859@gmail.com>
2026-03-08 13:19:47 +0900

    [DP-175] Swagger/OpenAPI 문서 세팅
    Auto-merged after CI passed. Branch: auto/feature/DP-175-swagger

---
+++ src/main/java/com/devpick/global/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.devpick.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("!test")
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("DevPick API")
+                        .description("개발자 성장형 통합 플랫폼 REST API 명세서")
+                        .version("v1.0"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("JWT Access Token")));
+    }
+}
