87c61fbe44873d5af6f55ccee1f48dae7af27228
nYeonG4001 <2371324@hansung.ac.kr>
2026-05-03 22:43:18 +0900

    fix: ScrapServiceTest eq import 누락 수정

--- src/test/java/com/devpick/domain/content/service/ScrapServiceTest.java
+++ src/test/java/com/devpick/domain/content/service/ScrapServiceTest.java
@@ -38,6 +38,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.verify;
